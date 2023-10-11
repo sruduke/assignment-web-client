d725238bb4e7d54bf08df82f7bf20b32baefd7b2
Sophia <97320246+sruduke@users.noreply.github.com>
2023-10-11 14:32:22 -0600

    test time

--- httpclient.py
+++ httpclient.py
@@ -24,8 +24,11 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+DEBUG = True
+
 GET = 0
 POST = 1
+LOCALHOST = "127.0.0.1"
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -34,13 +37,35 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    
+    def __repr__(self):
+        return f'Code: {self.code}\nBody:\n{self.body}'
 
 class HTTPClient(object):
-    def assign_host_port(self, url):
-        url_obj = urllib.parse(url)
 
-        self.port = url_obj.port
-        self.host = url_obj.hostname
+    def assign_host_port(self, url):
+        url_obj = urllib.parse.urlparse(url)
+
+        if DEBUG:
+            print(url_obj)
+
+        self.path = url_obj.path if url_obj.path else "/"
+
+        regex = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}\b"
+        if re.match(regex, url_obj.netloc):
+            self.host, self.port = url_obj.netloc.split(":")
+            self.port = int(self.port)
+
+            if DEBUG:
+                print(self.host, self.port)
+
+            return
+
+        #self.port = 443 if url_obj.scheme == "https" else 80
+        self.port = 80
+        #self.host = url_obj.netloc if url_obj.netloc[:4] == "www." else "www." + url_obj.netloc
+        self.host = url_obj.netloc
+        print(self.host, self.port)
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -70,17 +95,20 @@ class HTTPClient(object):
             
         return ""
     
-    def form_request(self, url, method, args):
+    def form_request(self, method, args):
         method_name = ['GET','POST'][method]
         nl = "\r\n"
 
-        req = f'{method_name} {url} HTTP/1.1{nl}Host: {self.host}{nl}'
+        req = f'{method_name} {self.path} HTTP/1.1{nl}Host: {self.host}'
         
         if args and len(args) > 0:
             encoded = urllib.parse.urlencode(args)
-            req += f'Content-Type: application/x-www-form-urlencoded{nl}Content-Length: {len(encoded)}{nl}{nl}{encoded}'
+            req += f'{nl}Content-Type: application/x-www-form-urlencoded{nl}Content-Length: {len(encoded)}{nl * 2}{encoded}'
         
-        return req.encode('utf-8')
+        if DEBUG:
+            print(req)
+
+        return req + (nl * 2)
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -98,14 +126,20 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        
+        try:
+            data = buffer.decode('utf-8')
+        except:
+            data = buffer.decode('iso-8859-1')
+
+        return data
 
     def handle_transaction(self, method, url, args=None):
         self.assign_host_port(url)
         self.connect(self.host, self.port)
 
         # deal with sending request
-        request = self.form_request(url, method, args)
+        request = self.form_request(method, args)
         self.sendall(request)
         self.socket.shutdown(socket.SHUT_WR)
 
@@ -113,7 +147,9 @@ class HTTPClient(object):
         data = self.recvall(self.socket)
         self.close()
 
-        print(data)
+        if DEBUG:
+            print(data)
+
         code = self.get_code(data)
         body = self.get_body(data)
         return HTTPResponse(int(code), body)
